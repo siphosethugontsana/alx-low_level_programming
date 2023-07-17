b76fb7587e939211d2ab5ad4c3cfbad476e39376
Your Name <you@email.com>
2023-07-17 16:25:08 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a programe that prints
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
