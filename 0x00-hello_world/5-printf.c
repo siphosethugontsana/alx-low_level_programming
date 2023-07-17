d79dd5dee3cc14280d30342005902afb3e92af60
Your Name <you@email.com>
2023-07-17 16:28:56 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for hello
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
