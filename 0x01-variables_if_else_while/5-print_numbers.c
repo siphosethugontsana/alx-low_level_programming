8838748fe8c2c05150f2e8fc9f99ff15e302f970
Your Name <you@email.com>
2023-07-17 17:55:18 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 9.
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
