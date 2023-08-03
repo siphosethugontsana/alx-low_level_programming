627c780af7eadff1a55b62b0a25f36e6aea0646c
Your Name <you@email.com>
2023-08-03 08:38:10 -0700

    df

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  pow_recursion - returns the value of x raised to the power of y
+ *  _pow_recursion - returns the value of x raised to the power of y
  *  @x: value to raise
  *  @y: power
  *  Return: result of the power
